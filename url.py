e7b624c8481978d7a473aa6073845d4a1e4a313f
Paweł S. Piotrowski <pawel.piotrowski@firma.o2.pl>
2014-08-25 22:57:21 +0200

    url validation

--- url.py
+++ url.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, abort
+from lepl.apps.rfc3696 import HttpUrl
 import redis, zlib
 
 app = Flask(__name__)
@@ -9,14 +10,18 @@ def redirect(key):
     if not redis.get(key):
         abort(404)
     else:
-        return redirect(target_url)
+        return redirect(redis.get(key))
 
 @app.route("/shorten/", methods=['POST'])
 def shorten():
     url = request.form['url']
-    key = zlib.crc32(url)
-    redis.set(key, url)
-    return str(key)
+    validator = HttpUrl()
+    if validator(url):
+        key = zlib.crc32(url)
+        redis.set(key, url)
+        return str(key)
+    else:
+        return ''
 
 if __name__ == "__main__":
     app.run(debug=True)
