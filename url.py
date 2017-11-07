78233c18029f88b217fcb311c6b7c50285cd04f4
Paweł S. Piotrowski <pawel.piotrowski@gmail.com>
2017-11-07 23:28:18 +0100

    Vue / Jinja delimiter conflict

--- url.py
+++ url.py
@@ -2,7 +2,20 @@ from flask import Flask, request, redirect, abort, render_template
 from flask_cors import CORS
 import redis, zlib, validators, os, socket
 
+# https://github.com/yymm/flask-vuejs
+class CustomFlask(Flask):
+  jinja_options = Flask.jinja_options.copy()
+  jinja_options.update(dict(
+    block_start_string='(%',
+    block_end_string='%)',
+    variable_start_string='((',
+    variable_end_string='))',
+    comment_start_string='(#',
+    comment_end_string='#)',
+  ))
+
 app = Flask(__name__)
+app = CustomFlask(__name__)
 CORS(app)
 redis = redis.StrictRedis()
 
